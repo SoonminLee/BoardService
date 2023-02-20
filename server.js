2568389072e323ff1e4d9a9d7a9fa31032ec0813
이순민 <tnsals4409@naver.com>
2023-02-20 19:05:42 +0900

    로그인/로그아웃 구현완료

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ MongoClient.connect(url, function (에러, client) {
   })
 })
 
+
 // 홈페이지로 이동
 app.get('/', function (요청, 응답) {
   응답.render(__dirname + '/views/index.ejs')
@@ -47,8 +48,8 @@ app.get('/', function (요청, 응답) {
 //===========================
 
 // 작성페이지로 이동
-app.get('/write', function (요청, 응답) {
-  응답.render(__dirname + '/views/write.ejs')
+app.get('/write', 로그인했냐, function (요청, 응답) {
+  응답.render(__dirname + '/views/write.ejs',{ 사용자 : 요청.user })
 });
 //=====================================
 
@@ -62,6 +63,80 @@ app.get('/login', function (요청, 응답) {
   응답.render('login.ejs')
 })
 //=====================================
+// /userIndex 페이지로 이동(로그인한사람만 입장)
+app.get('/userIndex',로그인했냐, function (요청, 응답) {
+  응답.render('userIndex.ejs', { 사용자 : 요청.user })
+})
+//=====================================
+
+
+
+// login 기능
+app.post('/login', passport.authenticate('local', {
+  failureRedirect: '/fail'
+}), function (요청, 응답) {
+  // 1. 로그인하면 아이디 비번 검사
+    응답.redirect('/userIndex')
+  
+})
+// logout 기능
+app.get('/logout', function (req, res){
+  // req.logout();
+  // res.redirect('/');
+  
+  req.session.destroy(() => {
+    res.clearCookie('connect.sid');
+    res.redirect('/');
+}); 
+})
+//==========================================
+
+
+function 로그인했냐(요청, 응답, next){
+  if (요청.user){
+      next()
+  } else {
+      // 응답.write("<script>('로그인 하고 오십쇼')</script>")
+      응답.send('로그인하고 오십쇼')
+      응답.redirect('/login')
+  }
+}
+
+// LocalStrategy 인증방법
+passport.use(new LocalStrategy({
+  usernameField: 'userId',
+  passwordField: 'userPw', //  유저가 입력한 아이디/비번 항목이 뭔지 정의(name속성)
+  session: true, // 로그인 후 세션을 저장할것인지
+  passReqToCallback: false, // 아이디/비번 말고도 다른 정보 검증시 true로 바꾸고 사용가능
+}, function (입력한아이디, 입력한비번, done) {  // 여기부터 끝까지는 아이디/비번을 DB와 비교하여 검증하는 코드
+  // console.log('입력한아이디랑입력한비번'+입력한아이디, 입력한비번);
+  db.collection('user').findOne({ userId: 입력한아이디 }, function (에러, 결과) {
+      if (에러) return done(에러)
+      // 여기부터 중요! DB에 아이디가 없을때 실행하고, 있긴한데 비번이 틀렸을때 실행(if문으로 분기)
+      // done(서버에러 : 보통 null , 성공시사용자DB데이터 : 아이디/비번 안맞을때 false, 에러메세지)
+      if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+      
+      if (createHashedPassword(입력한비번) == 결과.userPw) {
+          return done(null, 결과)
+      } else {
+          return done(null, false, { message: '비번틀렸어요' })
+      }
+  })
+}));
+
+// id를 이용하여 세션을 저장시키는 코드(로그인 성공시 발동)
+passport.serializeUser(function (user, done) {
+  done(null, user.userId)
+})
+
+// 이 세션 데이터를 가진 사람을 DB에서 찾아주세요(마이페이지 접속시 발동)
+passport.deserializeUser(function (아이디, done) {
+  // deserializeUser() : DB에서 위에 있던 user.id로 유저를 찾은 뒤에 유저 정보를 아래 done의 중괄호에 넣음
+  db.collection('user').findOne({ userId: 아이디 }, function (에러, 결과) {
+      done(null, 결과)
+  })
+})
+//==========================================
 
 // 작성페이지 DB에 저장
 app.post('/write', function (요청, 응답) {
@@ -91,11 +166,11 @@ app.post('/signUp', function (요청, 응답) {
   console.log('서버단 아디닉넴이멜' + 아이디, 닉네임, 이메일);
   // console.log(이름,아이디,비밀번호,닉네임,이메일)
   db.collection('user').findOne({ userId: 아이디 }, function (에러, 아이디결과) {
-    console.log('아이디결과' + 아이디결과)
+    // console.log('아이디결과' + 아이디결과)
     db.collection('user').findOne({ userNick: 닉네임 }, function (에러, 닉네임결과) {
-      console.log('닉네임결과' + 닉네임결과)
+      // console.log('닉네임결과' + 닉네임결과)
       db.collection('user').findOne({ userEmail: 이메일 }, function (에러, 이메일결과) {
-        console.log('이메일결과' + 이메일결과)
+        // console.log('이메일결과' + 이메일결과)
         if (아이디결과 == null && 닉네임결과 == null && 이메일결과 == null) {
           db.collection('user').insertOne({ userName: 이름, userId: 아이디, userPw: createHashedPassword(비밀번호), userNick: 닉네임, userEmail: 이메일 }, function (에러, 결과) { });
           응답.send("성공")
@@ -111,3 +186,5 @@ app.post('/signUp', function (요청, 응답) {
   })
 })
 //==========================================
+
+
