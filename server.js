5189ad889696d711505c17069937d2976219622d
Soonmin Lee <tnsals4409@naver.com>
2023-02-22 16:26:42 +0900

    server.js 주석 처리 추가

--- server.js
+++ server.js
@@ -1,49 +1,48 @@
+// 웹프레임워크 epress 사용하기위한 선언 코드 
 const express = require('express');
 const app = express();
+//===========================
+// 게시글 업로드 날짜 넣기 위해 사용한 라이브러리
 const dayjs = require('dayjs');
 const day = dayjs();
-
-
+//===========================
 // socket.io 셋팅코드
 const http = require('http').createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(http);
-
+//===========================
 app.use(express.urlencoded({ extended: true })) // POST 요청시 필요
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'))
+// ObjectId 라이브러리
 const ObjectId = require('mongodb').ObjectId;
+//===========================
+// 비밀번호 암호화하기위한 라이브러리 crypto
 const crypto = require('crypto');
 const createHashedPassword = (password) => {
   return crypto.createHash("sha512").update(password).digest("base64");
 };
-
+//===========================
 // passport 라이브러리 사용하기위한 코드
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
-
 app.use(session({ secret: '비밀코드', resave: true, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 //===========================
-
-
 // mongoDB 접속 코드
 var db;
 const url = `mongodb+srv://admin:xkfanem1!@cluster0.8wei56h.mongodb.net/?retryWrites=true&w=majority`
 const options = { useUnifiedTopology: true };
 const MongoClient = require('mongodb').MongoClient
 MongoClient.connect(url, function (에러, client) {
-
   db = client.db('BoardService');
-
   http.listen(8080, function () {
     console.log('listening on 8080');
   })
 })
-
-
+//===========================
 // 홈페이지로 이동
 app.get('/', function (요청, 응답) {
   var page = Number(요청.query.pageNum || 1);
@@ -65,13 +64,11 @@ app.get('/', function (요청, 응답) {
   })
 });
 //===========================
-
 // 작성페이지로 이동
 app.get('/write', 로그인했냐, function (요청, 응답) {
   응답.render(__dirname + '/views/write.ejs', { 사용자: 요청.user })
 });
 //=====================================
-
 // /signUp 페이지로 이동
 app.get('/signUp', function (요청, 응답) {
   응답.render('signUp.ejs')
@@ -83,7 +80,6 @@ app.get('/login', function (요청, 응답) {
 })
 //=====================================
 // /userIndex 페이지로 이동(로그인한사람만 입장)
-
 app.get('/userIndex', 로그인했냐, function (요청, 응답) {
   var page = Number(요청.query.pageNum || 1);
   var perPage = Number(요청.query.perPage || 10);
@@ -103,7 +99,6 @@ app.get('/userIndex', 로그인했냐, function (요청, 응답) {
   })
 });
 //=====================================
-
 
 //상세보기 페이지
 // detail로 접속하면 detail.ejs 보여주십쇼
